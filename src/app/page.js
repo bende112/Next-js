e54fe01a00a654d06aa4f6f617464f16d0e105f9
Bende <bende.nku@yahoo.com>
2024-01-22 10:17:38 +0000

    Next project

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>welcome to my Next.js Project</h1>
+      <p>This is my first page</p>
+      <p>Ignore the look of the page for now</p>
+
+    </div>
+  );
+}
